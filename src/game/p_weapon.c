cff44ace783d4e5030bccdbfdd1a85203819327c
Bela Kampis <belakampis@gmail.com>
2026-02-13 16:16:20 -0500

    Update p_weapon.c

--- src/game/p_weapon.c
+++ src/game/p_weapon.c
@@ -803,51 +803,76 @@ void Weapon_HyperBlaster_Fire(edict_t *ent)
 
     if (!(ent->client->buttons & BUTTON_ATTACK)) {
         ent->client->ps.gunframe++;
-    } else {
-        if (! ent->client->pers.inventory[ent->client->ammo_index]) {
-            if (level.framenum >= ent->pain_debounce_framenum) {
+    } 
+    else 
+    {
+        if (! ent->client->pers.inventory[ent->client->ammo_index]) 
+        {
+            if (level.framenum >= ent->pain_debounce_framenum) 
+            {
                 gi.sound(ent, CHAN_VOICE, gi.soundindex("weapons/noammo.wav"), 1, ATTN_NORM, 0);
                 ent->pain_debounce_framenum = level.framenum + 1 * BASE_FRAMERATE;
             }
             NoAmmoWeaponChange(ent);
-        } else {
+        } 
+        else 
+        {
             rotation = (ent->client->ps.gunframe - 5) * (M_PI / 3);
             offset[0] = -4 * sin(rotation);
             offset[1] = 0;
             offset[2] = 4 * cos(rotation);
 
-            if ((ent->client->ps.gunframe == 6) || (ent->client->ps.gunframe == 9))
+            if((ent->client->ps.gunframe == 6) || (ent->client->ps.gunframe == 9))
+            {
                 effect = EF_HYPERBLASTER;
+            }
             else
+            {
                 effect = 0;
-            if (deathmatch->value)
+            }
+
+            if(deathmatch->value)
+            {
                 damage = 15;
+            }
             else
+            {
                 damage = 20;
+            }
+
             Blaster_Fire(ent, offset, damage, true, effect);
-            if (!((int)dmflags->value & DF_INFINITE_AMMO))
+
+            if(!((int)dmflags->value & DF_INFINITE_AMMO))
+            {
                 ent->client->pers.inventory[ent->client->ammo_index]--;
+            }
 
             ent->client->anim_priority = ANIM_ATTACK;
-            if (ent->client->ps.pmove.pm_flags & PMF_DUCKED) {
+
+            if (ent->client->ps.pmove.pm_flags & PMF_DUCKED) 
+            {
                 ent->s.frame = FRAME_crattak1 - 1;
                 ent->client->anim_end = FRAME_crattak9;
-            } else {
+            } 
+            else 
+            {
                 ent->s.frame = FRAME_attack1 - 1;
                 ent->client->anim_end = FRAME_attack8;
             }
         }
 
         ent->client->ps.gunframe++;
-        if (ent->client->ps.gunframe == 12 && ent->client->pers.inventory[ent->client->ammo_index])
+        if(ent->client->ps.gunframe == 12 && ent->client->pers.inventory[ent->client->ammo_index])
+        {
             ent->client->ps.gunframe = 6;
+        }
     }
 
-    if (ent->client->ps.gunframe == 12) {
+    if (ent->client->ps.gunframe == 12) 
+    {
         gi.sound(ent, CHAN_AUTO, gi.soundindex("weapons/hyprbd1a.wav"), 1, ATTN_NORM, 0);
         ent->client->weapon_sound = 0;
     }
-
 }
 
 void Weapon_HyperBlaster(edict_t *ent)
