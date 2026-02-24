b566ab429d4e0aa57950f691b729f74b8d439237
Bela Kampis <belakampis@gmail.com>
2026-02-24 04:10:55 -0500

    Update openxr_interface.cpp

--- src/refresh/vkpt/openxr/openxr_interface.cpp
+++ src/refresh/vkpt/openxr/openxr_interface.cpp
@@ -3113,7 +3113,7 @@ extern "C"
 
 		glm::mat4 hmd_rotation_matrix = B * rotation_matrix_orig;
 
-		static bool normalize_it = true;
+		static bool normalize_it = false;
 
 		if(normalize_it)
 		{
@@ -3173,9 +3173,9 @@ extern "C"
 		view_pose.translation_.y -= head_position_WS.x;
 		view_pose.translation_.z += head_position_WS.y;
 
-		view_pose.translation_.x -= head_to_eye_WS.z;
-		view_pose.translation_.y -= head_to_eye_WS.x;
-		view_pose.translation_.z += head_to_eye_WS.y;
+		view_pose.translation_.x += head_to_eye_WS.x;
+		view_pose.translation_.y += head_to_eye_WS.y;
+		view_pose.translation_.z += head_to_eye_WS.z;
 
 		const glm::mat4 final_inv_view_matrix = view_pose.to_matrix();
 		memcpy(inv_view_matrix_ptr, &final_inv_view_matrix, sizeof(float) * 16);
